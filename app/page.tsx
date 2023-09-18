7d058dd692776612ad1f025e65cb63f167c1f802
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-09-18 22:09:28 +0200

    feat: win and draw logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,51 @@
 "use client";
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Cell from './components/Cell';
 
-
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 export default function Home() {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   const [go, setGo] = useState("O");
+  const [winningMessage, setWinningMessage] = useState("");
+  useEffect(() => {
+    winningCombos.forEach((combo) => {
+      const circleWins = combo.every((cell) => cells[cell] === "O");
+      const crossWins = combo.every((cell) => cells[cell] === "X");
+      if(circleWins){
+        setWinningMessage("O WINS !")
+      } else if (crossWins){
+        setWinningMessage("X WINS !")
+      }
+    })
+  }, [cells, winningMessage]);
+
+  useEffect(()=>{
+    if(cells.every((cell)=> cell !== "") && !winningMessage){
+      setWinningMessage("DRAW !");
+    }
+  }, [cells, winningMessage]);
+
   return (
     <div className='container'>
       <div className='gameboard'>
         {
           cells.map(
-            (cell, index) => <Cell id={index} go={go} setGo={setGo} key={index} cells={cells} setCells={setCells} cell={cell}/>
+            (cell, index) => <Cell id={index} go={go} setGo={setGo} key={index} cells={cells} setCells={setCells} cell={cell} winningMessage={winningMessage}/>
           )
         }
       </div>
-      <div>{`Now its ${go} turn !`}</div>
+      <div>{winningMessage}</div>
+      <div>{!winningMessage && `Now its ${go} turn !`}</div>
     </div>
   )
 }
