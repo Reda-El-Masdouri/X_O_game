14adc1a06bd7baf6539113fe696db415e9fe9189
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-09-18 21:36:15 +0200

    feat: cell component and click logic

---
+++ app/components/Cell.tsx
@@ -0,0 +1,34 @@
+import { Dispatch, SetStateAction } from "react";
+
+type CellProps = {
+  id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string[]>>;
+};
+
+const Cell = ({ go, setGo, id, cells, setCells }: CellProps) => {
+  
+  const handleClick = (e) => {
+    const notTaken = !cells[id];
+    if (notTaken) {
+      if (go === "O") {
+        handleCellChange("O");
+        setGo("X");
+      } else if (go === "X") {
+        handleCellChange("X");
+        setGo("O");
+      }
+    }
+  };
+  const handleCellChange = (cellToChange: string) => {
+    let copyCells = [...cells];
+    copyCells[id] = cellToChange;
+    setCells(copyCells);
+  };
+
+  return <div className="square" onClick={handleClick}></div>;
+};
+
+export default Cell;
